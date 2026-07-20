88c9476d8d81e5372411c9842bac9a23be2839bd
MSigging <msigging@gmail.com>
2026-07-20 20:11:55 +1000

    Create MealBooking Class

--- AT1_DiningFeature/MealBooking.js
+++ AT1_DiningFeature/MealBooking.js
@@ -1,1 +1,30 @@
+/*
+  Program: Dining Meal Booking Feature
+  Student Name: Megdelene SIGGING
+  Student ID: 240162
+  Date: 17 July 2026
+  
+*/
 
+
+class MealBooking {
+  // Declare private fields
+  #studentId;
+  #studentName;
+  #mealDate;
+  #mealType;
+  #quantity;
+  #dietaryNote;
+  #bookingStatus;
+
+  constructor({ studentId, studentName, mealDate, mealType, quantity, dietaryNote = "None" }) {
+    this.#studentId = studentId;
+    this.#studentName = studentName;
+    this.#mealDate = mealDate;
+    this.#mealType = mealType;
+    this.#quantity = quantity;
+    this.#dietaryNote = dietaryNote;
+    this.#bookingStatus = "Pending"; // Set to Pending by default
+  }
+
+  
